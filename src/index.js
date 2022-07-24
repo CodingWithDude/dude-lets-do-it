08ef32a14a26db1788890f1e21d4d2ef093209f9
CodingWithDude <codingwithdude@gmail.com>
2022-07-24 01:32:59 -0700

    Added Local Storage
    added eslint with air bnb preset
    changes prettier to use single quotes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,58 @@
-import "./styles/reset.sass";
-import "./styles/main.sass";
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+import './styles/reset.sass';
+import './styles/main.sass';
 
-const listsContainer = document.querySelector("[data-lists]");
-const newListForm = document.querySelector("[data-new-list-form]");
-const newListInput = document.querySelector("[data-new-list-input]");
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = [];
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+const lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
-newListForm.addEventListener("submit", (e) => {
+listsContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listID;
+    saveAndRender();
+  }
+});
+
+newListForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const listName = newListInput.value;
-  if (listName == null || listName === "") return;
+  if (listName == null || listName === '') return;
   const list = createList(listName);
   newListInput.value = null;
   lists.push(list);
-  render();
+  saveAndRender();
 });
 
 function createList(name) {
-  return { id: Date.now().toString(), name: name, tasks: [] };
+  return { id: Date.now().toString(), name, tasks: [] };
+}
+
+function saveAndRender() {
+  render();
+  save();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
 function render() {
   clearElement(listsContainer);
   lists.forEach((list) => {
-    const listElement = document.createElement("li");
+    const listElement = document.createElement('li');
     listElement.dataset.listID = list.id;
-    listElement.classList.add("list-name");
+    listElement.classList.add('list-name');
     listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list');
+    }
     listsContainer.appendChild(listElement);
   });
 }
